5fa680e06f257564a85e2ce5ada5584815266cb0
Cyan <hectorgd711@gmail.com>
2026-05-07 17:21:49 +0200

    Conexion con base de datos

--- src/Proyecto.java
+++ src/Proyecto.java
@@ -1,4 +1,17 @@
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 public class Proyecto {
 
+	public static void main (String []args) {
+		String uri="mongodb+srv://hectorgd711_db_user:ReFcQMhbNqdAvOGJ"
+				+ "@proyectostreamersg6.5osvic5.mongodb.net/?appName=ProyectoStreamersG6";
+		
+		
+		MongoClient mongoClient = MongoClients.create(uri);
+		MongoDatabase mongoDatabase =  mongoClient.getDatabase("ProyectoStreamersG6");
+		
+	}
+		
 }
